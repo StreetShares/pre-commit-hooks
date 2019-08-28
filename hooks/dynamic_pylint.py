1763788111ad7d337492fb51a9f48678b81fdbd5
Van Johnson <vjohnson@streetshares.com>
2019-08-28 09:50:54 -0400

    INFRA-78: Adding Dockerfiles for python2,3 version of pylint

--- hooks/dynamic_pylint.py
+++ hooks/dynamic_pylint.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 """StreetShares Dynamic Pylint Wrapper
-Runs pylint based on the PYTHON_VERSION of the repo.  If within the
-service.config, PYTHON_VERSION is set, then the appropriate version of pylint
-will be executed against the code base, else fall back to the current python in
-the PATH.
+Runs pylint within a docker container.  If within the service.config
+PYTHON_VERSION is set, then the appropriate version of pylint will be executed
+against the code base, else skip for now.
 
 Return Codes:
     0 - Perfect Linting! 10/10 Gold Stars!
@@ -17,6 +16,8 @@ from __future__ import print_function
 import argparse
 import re
 import subprocess
+import os
+import sys
 
 def _get_python_major_version(service_config_file):
     regex = re.compile(r"^PYTHON_VERSION\s*=\s*([^.]).*$")
@@ -26,6 +27,7 @@ def _get_python_major_version(service_config_file):
                 result_match = re.search(regex, line)
                 if result_match:
                     result = result_match.group(1)
+                    break
                 else:
                     result = None
     except IOError:
@@ -41,14 +43,18 @@ def _run_cmd(cmd):
     """
     proc = subprocess.Popen(
         cmd,
-        shell=True
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE
     )
-    proc.communicate()
-    return proc.returncode
+    output = proc.communicate()[0]
+    proc.wait()
+
+    return(proc.returncode, output.decode("utf-8"))
 
 
 def main(argv=None):
-    """Runs appropriate pylint.
+    """Runs appropriate pylint from docker container.
     Returns:
         STDOUT of pylint or pylint not found.
     """
@@ -56,21 +62,57 @@ def main(argv=None):
     parser.add_argument(
         'filenames', nargs='*',
         help='Filenames pre-commit believes are changed.',
+        default='.'
     )
     service_config_file = 'k8s/service.config'
     python_major_version = _get_python_major_version(service_config_file)
     args = parser.parse_args(argv)
 
-    python3_pylint = '/usr/local/bin/python3 -mpylint {}'.format(' '.join(args.filenames))
-    python_pylint = '/usr/bin/python -mpylint {}'.format(' '.join(args.filenames))
-
-    if python_major_version == '3':
-        return_code = _run_cmd(python3_pylint)
-    elif python_major_version == '2':
-        return_code = _run_cmd(python_pylint)
+    # Determine which docker image to use based on the major version of python
+    # for the repo.
+    if python_major_version == '2':
+        tag = '1.9.5'
+        image_name = 'pylint-pre-commit:' + tag
+    elif python_major_version == '3':
+        tag = '2.3.1'
+        image_name = 'pylint-pre-commit:' + tag
     else:
-        print('PYTHON_VERSION not set correctly in your {}'.format)
-        return_code = 3
+        print('PYTHON_VERSION not set in {}.  Skipping...'.format(
+            service_config_file
+        ), file=sys.stderr)
+        # For now we are going to skip. PYTHON_VERSION  needs to be set
+        # everywhere, but that will not happen immediatly.  Switch this over
+        # after some time out in the wild.
+        return_code = 0
+        return return_code
+
+    if len(args.filenames) > 1:
+        python_files = ''
+        for file in args.filenames:
+            python_files += file + ' '
+    else:
+        python_files = args.filenames
+
+    _, image_available = _run_cmd('docker images --format "{{.Repository}}:{{.Tag}}"')
+    if image_name not in image_available:
+        print('{} docker image not found.  Building...'.format(image_name))
+        _run_cmd('docker build -t {image_name} {dockerfile_location}'.format(
+            image_name=image_name,
+            dockerfile_location=os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                '../docker/pylint/{}'.format(tag)
+            )
+        ))
+
+    docker_cmd = ('docker run --rm -v {mount_origin}:/data {image} {files}'.format(
+        mount_origin=os.getcwd(),
+        image=image_name,
+        files=python_files
+    ))
+
+    return_code, output = _run_cmd(docker_cmd)
+
+    print(output)
 
     return return_code
 
